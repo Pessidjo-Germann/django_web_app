81d14ff4760a09d75ab1a55a6f22b21e63a63be7
Germann Pessidjo <germannpessidjo6@gmail.com>
2025-02-07 23:51:22 +0100

    models

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,21 @@
+from django.http import HttpResponse
+
 from django.shortcuts import render
 
 # Create your views here.
+
+def hello(request):
+    return HttpResponse('<h1>Hello, World!</h1>')
+
+def view(request):
+    return HttpResponse('<h1>A propos</h1> Nous adorons Merch')
+
+def about(request):
+    return HttpResponse('A propos')
+
+def listings(request):
+    return HttpResponse('<h1>liste des articles</h1> ')
+
+
+def contacts(request):
+    return HttpResponse('<h1>Nous contacter!</h1> ')
