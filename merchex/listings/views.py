e94e7799706e6662d966dfa7b339d7f6a351a24f
Germann Pessidjo <germannpessidjo6@gmail.com>
2025-02-08 01:06:20 +0100

    add model Announce

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,11 +1,19 @@
 from django.http import HttpResponse
 
 from django.shortcuts import render
-
+from listings.models import Band , Announce
 # Create your views here.
 
 def hello(request):
-    return HttpResponse('<h1>Hello, World!</h1>')
+    bands= Band.objects.all()
+    return HttpResponse(f"""
+        <h1>Mes groupes preferes sont :</h1>
+        <ul>
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+            <li>{bands[2].name}</li
+                        </ul>
+""")
 
 def view(request):
     return HttpResponse('<h1>A propos</h1> Nous adorons Merch')
@@ -14,7 +22,14 @@ def about(request):
     return HttpResponse('A propos')
 
 def listings(request):
-    return HttpResponse('<h1>liste des articles</h1> ')
+    announces=Announce.objects.all()
+    return HttpResponse(f"""
+            <h1>liste des articles</h1>
+            <ul>
+                <li>{announces[0].title}</li>
+                <li>{announces[1].title}</li>
+                <li>{announces[2].title}</li></ul>
+""")
 
 
 def contacts(request):
